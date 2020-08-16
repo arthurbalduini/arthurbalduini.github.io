9e60e577f330b3cbfefccf5be8921731d1cf8caa
Arthur Balduini <arthurbalduini@gmail.com>
2020-08-16 19:37:46 -0300

    Updates

---
+++ precache-manifest.df4b7c923e0d3ea8af0d9326d2c17556.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf8d86f49f383ec9afea9c7c63e865e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfcf388beed499ace5b8",
+    "url": "/static/css/main.0a8f6339.chunk.css"
+  },
+  {
+    "revision": "a50e7e1935f0b3f21a50",
+    "url": "/static/js/2.4b67183b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b67183b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcf388beed499ace5b8",
+    "url": "/static/js/main.040c5907.chunk.js"
+  },
+  {
+    "revision": "aa0edd1ded475b7ff765",
+    "url": "/static/js/runtime-main.3bcc2a15.js"
+  },
+  {
+    "revision": "31c2153c0530e32553b31a49b3d70736",
+    "url": "/static/media/ArchitectsDaughter-Regular.31c2153c.ttf"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "687e1ca20605331b5ce870bbcf0f5a13",
+    "url": "/static/media/infinity-monkey-01.687e1ca2.jpg"
+  }
+]);
