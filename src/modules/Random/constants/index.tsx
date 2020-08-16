c67734d41d0dc980193d272c3136f62c11fc8d94
Arthur Balduini <arthurbalduini@gmail.com>
2020-08-16 20:57:46 -0300

    Fixes neymar info and styling

--- src/modules/Random/constants/index.tsx
+++ src/modules/Random/constants/index.tsx
@@ -3,7 +3,11 @@ import { NeymarStatsInterfaceArray } from "../interfaces";
 export const neymarStats: NeymarStatsInterfaceArray = [
   {
     key: "Salário do Neymar:",
-    value: "€ 36,8 milhões",
+    value: "€ 36,8 milhões por ano",
+  },
+  {
+    key: "Um ano:",
+    value: "525600 minutos",
   },
   {
     key: "Tempo caído:",
