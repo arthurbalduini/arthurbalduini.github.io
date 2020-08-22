c144f9f2fcecdf4d257976de77f45762d660dc8f
Arthur Balduini <arthurbalduini@gmail.com>
2020-08-22 00:27:46 -0300

    adds first animation

--- src/modules/Home/pages/index.tsx
+++ src/modules/Home/pages/index.tsx
@@ -1,14 +1,38 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useEffect } from "react";
 import "./index.css";
 import Layout from "../../Layout";
+import { TweenMax, Power2 } from "gsap";
 
 const componentClassname = "home";
 
 const Home: FC = () => {
+  let item1: HTMLHeadingElement | any = useRef();
+  let item2: HTMLHeadingElement | any = useRef();
+
+  useEffect(() => {
+    TweenMax.from(item1, { x: -900, duration: 3, ease: Power2.easeOut });
+    TweenMax.from(item2, { x: 900, duration: 3, ease: Power2.easeOut });
+  }, []);
+
   return (
     <Layout>
       <div className={componentClassname}>
-        <h1>Teste 123</h1>
+        <div className={`${componentClassname}__test`}>
+          <h1
+            ref={(e) => {
+              item1 = e;
+            }}
+          >
+            Em construção...
+          </h1>
+          <h1
+            ref={(e) => {
+              item2 = e;
+            }}
+          >
+            Aguarde, mas não muito.
+          </h1>
+        </div>
       </div>
     </Layout>
   );
