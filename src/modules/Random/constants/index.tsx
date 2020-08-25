baec215122c2332fa7c7eca2a6bee28932313017
Arthur Balduini <arthurbalduini@gmail.com>
2020-08-24 23:51:11 -0300

    uses single quotes

--- src/modules/Random/constants/index.tsx
+++ src/modules/Random/constants/index.tsx
@@ -1,20 +1,20 @@
-import { NeymarStatsInterfaceArray } from "../interfaces";
+import { NeymarStatsInterfaceArray } from '../interfaces';
 
 export const neymarStats: NeymarStatsInterfaceArray = [
   {
-    key: "Salário do Neymar:",
-    value: "€ 36,8 milhões por ano",
+    key: 'Salário do Neymar:',
+    value: '€ 36,8 milhões por ano',
   },
   {
-    key: "Um ano:",
-    value: "525600 minutos",
+    key: 'Um ano:',
+    value: '525600 minutos',
   },
   {
-    key: "Tempo caído:",
-    value: "14 minutos",
+    key: 'Tempo caído:',
+    value: '14 minutos',
   },
   {
-    key: "Dinheiro ganho no chão:",
-    value: "€ 980,21",
+    key: 'Dinheiro ganho no chão:',
+    value: '€ 980,21',
   },
 ];
